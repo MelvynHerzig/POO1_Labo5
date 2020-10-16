b6531466d83d7f8b32bb90dfb0048d349c82b959
Forestier <quentin.forestier@heig-vd.ch>
2020-10-16 14:12:04 +0200

    Correction + tests

--- src/Matrice.java
+++ src/Matrice.java
@@ -1,6 +1,7 @@
 /**
  * Class implémentant des matrices de taille personnalisable.
  * Le contenu est défini modulo N.
+ *
  * @author Forestier Quentin, Herzig Melvyn
  * @version 14.10.2020
  */
@@ -12,16 +13,17 @@ public class Matrice
     /**
      * Constructeur, génère la matrice d'après les dimensions en paramètre
      * ainsi que le contenu d'après le modulo.
-     * @param aHeight Heuteur de la matrice.
-     * @param aWidth Largeur de la matrice.
+     *
+     * @param aHeight  Heuteur de la matrice.
+     * @param aWidth   Largeur de la matrice.
      * @param aModulus Modulo du contenu.
-     * @throws IllegalArgumentException Dans le cas d'une dimension négative où
-     *         d'un modulo inférieur à 1.
+     * @throws RuntimeException Dans le cas d'une dimension négative où
+     *                          d'un modulo inférieur à 1.
      */
     public Matrice(int aHeight, int aWidth, int aModulus)
     {
-        if(aHeight < 0 || aWidth < 0 || aModulus < 1)
-            throw new IllegalArgumentException();
+        if (aHeight < 0 || aWidth < 0 || aModulus < 1)
+            throw new RuntimeException("Argument(s) invalide(s)");
 
         modulus = aModulus;
 
@@ -39,7 +41,8 @@ public class Matrice
     /**
      * Constructeur de matrice carrée d'après la taille en paramètre et
      * génère le contenu d'après le modulo.
-     * @param size Hauteur et largeur de la matrice.
+     *
+     * @param size     Hauteur et largeur de la matrice.
      * @param aModulus Modulo du contenu.
      */
     public Matrice(int size, int aModulus)
@@ -50,21 +53,23 @@ public class Matrice
     /**
      * Constructeur créée l'objet matrice d'après le tableau bidimensionnel
      * en paramètre. Corrige son contenu d'après le module.
-     * @param aMatrice Matrice à integrer.
+     *
+     * @param aMatrice Matrice à intégrer.
      * @param aModulus Module à appliquer.
-     * @throws IllegalArgumentException Dans le cas d'un modulo inférieur à 1.
+     * @throws RuntimeException Dans le cas d'un modulo inférieur à 1.
      */
     public Matrice(int aMatrice[][], int aModulus)
     {
-        if(aModulus < 1) throw new IllegalArgumentException();
+        if (aModulus < 1)
+            throw new RuntimeException("Le modulo est < 1");
 
         modulus = aModulus;
         matrice = aMatrice;
 
         // Corrige le contenu pour respecter le modulo.
-        for (int M = 0; M < this.height(); ++M)
+        for (int M = 0; M < this.getHeight(); ++M)
         {
-            for (int N = 0; N < this.width(); ++N)
+            for (int N = 0; N < this.getWidth(); ++N)
             {
                 matrice[M][N] = Math.floorMod(matrice[M][N], modulus);
             }
@@ -72,52 +77,62 @@ public class Matrice
     }
 
     /**
-     * Retourne la taille de la matrice.
-     * @return Taille de la matrice.
+     * Retourne la hauteur de la matrice.
+     *
+     * @return Hauteur de la matrice.
      */
-    public int height()
+    public int getHeight()
     {
         return matrice.length;
     }
 
     /**
      * Retourne la largeur de la matrice
+     *
      * @return Largeur de la matrice.
      */
-    public int width()
+    public int getWidth()
     {
-        return matrice[0].length;
+
+        return matrice.length == 0 ? 0 : matrice[0].length;
     }
 
+
     /**
-     * Affiche le contenu de la matrice
+     * Retourne le modulo de la matrice
+     *
+     * @return Modulo de la matrice
      */
-    public void displayContent()
+    public int getModulus()
     {
-        for (int M = 0; M < this.height(); ++M)
-        {
-            for (int N = 0; N < this.width(); ++N)
-            {
-                System.out.print(matrice[M][N]);
-            }
-            System.out.print('\n');
-        }
+        return this.modulus;
     }
 
     /**
      * Assemble les caractéristiques de la matrice pour les afficher.
+     *
      * @return String formaté avec les caractéristiques de la matrice.
      */
     @Override
     public String toString()
     {
-        return "Matrice de hauteur " + this.height() +
-               " de largeur " + this.width() +
-               " de module " + modulus;
+        if (getHeight() == 0 || getWidth() == 0) return "Matrice vide";
+        StringBuilder result = new StringBuilder();
+        for (int M = 0; M < this.getHeight(); ++M)
+        {
+            for (int N = 0; N < this.getWidth(); ++N)
+            {
+                result.append(matrice[M][N]);
+            }
+            result.append('\n');
+        }
+        return result.toString();
+
     }
 
     /**
      * Additionne la matrice courante avec la matrice m.
+     *
      * @param m Matrice, seconde opérande.
      * @return Matrice résultante de la somme.
      */
@@ -128,6 +143,7 @@ public class Matrice
 
     /**
      * Soustrait la matrice courante avec la matrice m.
+     *
      * @param m Matrice, seconde opérande.
      * @return Matrice résultante de la différence.
      */
@@ -138,6 +154,7 @@ public class Matrice
 
     /**
      * Multiple la matrice courante avec la matrice m.
+     *
      * @param m Matrice, seconde opérande.
      * @return Matrice résultante du produit.
      */
@@ -149,28 +166,33 @@ public class Matrice
     /**
      * Fonction principale de génération d'une matrice issue de deux matrices
      * combinées, la matrice courante et la matrice m.
-     * @param m Matrice, seconde opérande.
+     *
+     * @param m        Matrice, seconde opérande.
      * @param operator Opération à appliquer entre les éléments Amn Bmn.
      * @return Retourne la matrice résultante de l'opération.
      */
     private Matrice operation(Matrice m, Operator operator)
     {
-        if (this.modulus != m.modulus) throw new RuntimeException();
+        if (this.modulus != m.modulus)
+            throw new RuntimeException("Les modulos ne sont pas égaux");
 
-        Matrice result = new Matrice(Math.max(this.height(), m.height()),
-                                     Math.max(this.width(), m.width()),
-                                     this.modulus);
+        Matrice result = new Matrice(Math.max(this.getHeight(), m.getHeight()),
+                Math.max(this.getWidth(), m.getWidth()),
+                this.modulus);
 
         int v1, v2;
-        for(int M = 0; M < result.height(); ++M)
+        for (int M = 0; M < result.getHeight(); ++M)
         {
-            for (int N = 0; N < result.width(); ++N)
+            for (int N = 0; N < result.getWidth(); ++N)
             {
                 // Vérification si la case MxN est dans les tableaux.
-                v1 = (M < this.height()  && N < this.width()) ? this.matrice[M][N] : 0;
-                v2 = (M < m.height()     && N < m.width()   ) ? m.matrice[M][N]    : 0;
+                v1 = (M < this.getHeight() && N < this.getWidth()) ?
+                        this.matrice[M][N] : 0;
+                v2 = (M < m.getHeight() && N < m.getWidth()) ?
+                        m.matrice[M][N] : 0;
 
-                result.matrice[M][N] = Math.floorMod(operator.apply(v1, v2), modulus);
+                result.matrice[M][N] = Math.floorMod(operator.apply(v1, v2),
+                        modulus);
             }
         }
         return result;
@@ -179,6 +201,7 @@ public class Matrice
 
 /**
  * Enum des opérations applicables sur les Matrices
+ *
  * @author Forestier Quentin, Herzig Melvyn
  * @version 14.10.2020
  *
@@ -214,6 +237,7 @@ enum Operator
 
     /**
      * Applique une opération sur x1 et x2.
+     *
      * @param x1 Premier nombre, élément de la première matrice.
      * @param x2 Second nombre, élément de la seconde matrice.
      * @return Résultat de l'opération entre x1 et x2.
--- src/Test.java
+++ src/Test.java
@@ -7,51 +7,179 @@
  * Deux matrices de contenu aléatoire seront générées et testées.
  * D'autres tests sont effectué avec des matrices prédéterminées pour
  * valider les cas limites.
+ *
  * @author Forestier Quentin, Herzig Melvyn
  * @version 14.10.2020
  */
 public class Test
 {
-   /**
-    * Fonction principale de test et entrée du programme.
-    * @param args Dimensions et module des matrices à créer
-    * @throws IllegalArgumentException Dans le cas d'une dimension négative où
-    *         d'un modulo inférieur à 1.
-    */
-   public static void main(String[] args)
-   {
-      if(args.length != 5) throw new IllegalArgumentException("Args: M1 N1 M2 N2 module");
-      int M1 =  Integer.parseInt(args[0]);
-      int N1 =  Integer.parseInt(args[1]);
-      int M2 =  Integer.parseInt(args[2]);
-      int N2 =  Integer.parseInt(args[3]);
 
-      // Tests selon donnée.
-      Matrice one = new Matrice(new int[][]{
-              {1, 3, 1, 1},
-              {3, 2, 4, 2},
-              {1, 0, 1, 0}}, 5);
 
-      Matrice two = new Matrice(new int[][]{
-              {1, 4, 2, 3, 2},
-              {0, 1, 0, 4, 2},
-              {0, 0, 2, 0, 2}}, 5);
+    public static void testStatic()
+    {
+        int modulus = 5;
 
-      System.out.println("The modulus is 5\n");
+        Matrice one = new Matrice(new int[][]{
+                {1, 3, 1, 1},
+                {3, 2, 4, 2},
+                {1, 0, 1, 0}
+        }, modulus);
 
-      System.out.println("one");
-      one.displayContent();
+        Matrice two = new Matrice(new int[][]{
+                {1, 4, 2, 3, 2},
+                {0, 1, 0, 4, 2},
+                {0, 0, 2, 0, 2}
+        }, modulus);
 
-      System.out.println("\ntwo");
-      two.displayContent();
+        System.out.println("The modulus is " + modulus + "\n");
 
-      System.out.println("\none + two");
-      one.add(two).displayContent();
+        System.out.println("one");
+        System.out.println(one);
 
-      System.out.println("\none - two");
-      one.sub(two).displayContent();
+        System.out.println("\ntwo");
+        System.out.println(two);
 
-      System.out.println("\none x two");
-      one.prod(two).displayContent();
-   }
+        System.out.println("\none + two");
+        System.out.println(one.add(two));
+
+        System.out.println("\none - two");
+        System.out.println(one.sub(two));
+
+        System.out.println("\none x two");
+        System.out.println(one.prod(two));
+    }
+
+    public static void testUserInput(int M1, int N1, int M2, int N2,
+                                     int modulus)
+    {
+
+        Matrice three = new Matrice(M1, N1, modulus);
+
+        Matrice four = new Matrice(M2, N2, modulus);
+
+        System.out.println("The modulus is " + modulus + "\n");
+
+        System.out.println("three");
+        System.out.println(three);
+
+        System.out.println("\nfour");
+        System.out.println(four);
+
+        System.out.println("\nthree + four");
+        System.out.println(three.add(four));
+
+        System.out.println("\nthree - four");
+        System.out.println(three.sub(four));
+
+        System.out.println("\nthree x four");
+        System.out.println(three.prod(four));
+    }
+
+    public static void testLimitCase()
+    {
+        try
+        {
+            System.out.println("Test de matrice avec une taille négative :");
+            Matrice negSize = new Matrice(-1, 4);
+
+        }
+        catch (RuntimeException e)
+        {
+            System.out.println(e);
+        }
+
+        System.out.println("\n");
+
+        try
+        {
+            System.out.println("Test de matrice avec un modulo incorrecte :");
+            Matrice badModulus = new Matrice(
+                    new int[][]{{1, 2, 3}, {2, 3, 4}}, 0);
+        }
+        catch (RuntimeException e)
+        {
+            System.out.println(e);
+        }
+
+        System.out.println("\n");
+
+        try
+        {
+            System.out.println("Test d'une opération avec un modulo différent" +
+                    " :");
+            Matrice m1 = new Matrice(2, 5);
+            Matrice m2 = new Matrice(5, 4);
+
+            m1.add(m2);
+        }
+        catch (RuntimeException e)
+        {
+            System.out.println(e);
+        }
+
+        System.out.println("\n");
+
+        try
+        {
+            System.out.println("Test création d'une matrice vide + addition :");
+            Matrice m1 = new Matrice(0, 0, 4);
+            Matrice m2 = new Matrice(3, 4, 4);
+
+            System.out.println(m1);
+            System.out.println(m2);
+
+            System.out.println(m2.sub(m1));
+        }
+        catch (RuntimeException e)
+        {
+            System.out.println(e);
+        }
+
+        System.out.println("\n");
+
+        try
+        {
+            System.out.println("Test création d'une matrice vide à partir " +
+                    "d'un tableau vide");
+            int[][] tbl = {};
+            Matrice vide = new Matrice(tbl, 6);
+
+            System.out.println(vide);
+        }
+        catch (RuntimeException e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    /**
+     * Fonction principale de test et entrée du programme.
+     *
+     * @param args Dimensions et module des matrices à créer
+     * @throws IllegalArgumentException Dans le cas d'une dimension négative où
+     *                                  d'un modulo inférieur à 1.
+     */
+    public static void main(String[] args)
+    {
+        if (args.length != 5)
+            throw new IllegalArgumentException("Args: M1 N1 M2 N2 module");
+        int M1 = Integer.parseInt(args[0]);
+        int N1 = Integer.parseInt(args[1]);
+        int M2 = Integer.parseInt(args[2]);
+        int N2 = Integer.parseInt(args[3]);
+
+        int modulus = Integer.parseInt(args[4]);
+
+        // Tests selon donnée.
+
+        System.out.println("------------ Test de la donnée ------------");
+        testStatic();
+
+        System.out.println("\n------------ Test de l'entrée utilisateur " +
+                "------------");
+        testUserInput(M1, N1, M2, N2, modulus);
+
+        System.out.println("\n------------ Test de cas limite ------------");
+        testLimitCase();
+    }
 }
